8831c864eba0fdcb9d4a05137105d64002b173a5
Mert Ozan Kahraman <mozankahraman69@gmail.com>
2023-08-10 16:18:29 +0300

    Repository added

--- app/src/main/java/com/mertozan/moviescompose/presantation/MovieViewModel.kt
+++ app/src/main/java/com/mertozan/moviescompose/presantation/MovieViewModel.kt
@@ -1,8 +1,8 @@
 package com.mertozan.moviescompose.presantation
 
 import androidx.lifecycle.ViewModel
-import com.mertozan.moviescompose.data.api.MovieService
 import com.mertozan.moviescompose.data.model.Movie
+import com.mertozan.moviescompose.data.repository.MovieRepository
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
@@ -13,20 +13,20 @@ import javax.inject.Inject
 
 @HiltViewModel
 class MovieViewModel @Inject constructor(
-    private val movieService: MovieService
-): ViewModel() {
+    private val movieRepository: MovieRepository
+) : ViewModel() {
 
-    private val _popularMovies= MutableStateFlow(emptyList<Movie>())
+    private val _popularMovies = MutableStateFlow(emptyList<Movie>())
     val popularMovies = _popularMovies.asStateFlow()
 
     init {
         getPopularMovies()
     }
 
-    fun getPopularMovies() {
+    private fun getPopularMovies() {
         CoroutineScope(Dispatchers.IO).launch {
-            val response = movieService.getPopularMovies()
-            _popularMovies.value = response.movieResults
+            movieRepository.getAllPopularMovies()
+            _popularMovies.value = movieRepository.repoPopularMovies.value
         }
     }
 
