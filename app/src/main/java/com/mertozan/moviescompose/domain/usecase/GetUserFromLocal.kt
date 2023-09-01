a7d98a88c1f13cb29149543a50fd54bc24debfe1
Mert Ozan Kahraman <mozankahraman69@gmail.com>
2023-09-01 11:29:12 +0300

    Re-factoring 01 done

---
+++ app/src/main/java/com/mertozan/moviescompose/domain/usecase/GetUserFromLocal.kt
@@ -0,0 +1,13 @@
+package com.mertozan.moviescompose.domain.usecase
+
+import com.mertozan.moviescompose.domain.model.UserModel
+import com.mertozan.moviescompose.domain.repository.MovieRepository
+import javax.inject.Inject
+
+class GetUserFromLocal @Inject constructor(
+    private val movieRepository: MovieRepository
+) {
+    suspend operator fun invoke() : UserModel{
+        return movieRepository.getUserFromLocale()
+    }
+}
